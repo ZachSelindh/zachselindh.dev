2fabb33807561fb4c6c7bc2a6bb0ae565fd9572a
Zach Selindh <zachselindh@gmail.com>
2020-01-02 19:14:35 -0600

    API verified using Postman.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("API Hit");
+  res.send({ msg: "API Hit" });
 });
 
 module.exports = router;
