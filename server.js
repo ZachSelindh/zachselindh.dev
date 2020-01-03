a5a6f8df662764610a54f80075ba7c8186f7ddeb
Zach Selindh <zachselindh@gmail.com>
2020-01-02 18:51:37 -0600

    Added React-app template in client.

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Express Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "client/public")));
+
+/* const routes = require("./routes");
+app.use(routes); */
+
+/* const connection = require("./config/connection");
+
+connection
+  .then(() => console.log("Database connected"))
+  .catch(err => console.log(err)); */
+
+const PORT = process.env.PORT || 3001;
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
