fa64fb1fbf8f8967d6851bd37e71d090f280e427
Zach Selindh <zachselindh@gmail.com>
2020-01-04 09:56:03 -0600

    Added findAll to API.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Project = require("../../models/Project");
 
 router.get("/", (req, res) => {
   res.send({ msg: "API Hit" });
 });
+
+router.get("/all"),
+  (req, res) => {
+    Project.find({})
+      .then(res => res.send())
+      .catch(err => res.json(err));
+  };
 
 module.exports = router;
