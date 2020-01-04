eaaa17d7212874d089e0216db0d6800727ab5277
Zach Selindh <zachselindh@gmail.com>
2020-01-03 18:34:58 -0600

    Added code for background image and wrapper component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Wrapper from "./Components/Wrapper";
 
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>Test</div>;
+    return (
+      <Wrapper>
+        <div>Test</div>
+      </Wrapper>
+    );
   }
 }
 
