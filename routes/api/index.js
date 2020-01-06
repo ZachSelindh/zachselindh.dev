3b4fe7ec97291fd0bcec8f7f38003ab6b84dc488
Zach Selindh <zachselindh@gmail.com>
2020-01-05 18:22:58 -0600

    Commenting express router API routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Project = require("../../models/Project");
 
-router.get("/", (req, res) => {
+// API tets route
+router.get("/test", (req, res) => {
   res.send({ msg: "API Hit" });
 });
 
+// Get all projects in database
 router.get("/allprojects", (req, res) => {
   Project.find({})
     .then(foundProjects => res.send(foundProjects))
     .catch(err => res.json(err));
 });
 
+// Uses Route Parameters to identify and query the technology
+// as part of the API route
 router.get("/tech/:technology", (req, res) => {
   Project.find({ technologies: req.params.technology })
     .sort({ date_completed: -1 })
