896ae1eb3b4f483f61fcfa8476b07040507d8641
Zach Selindh <zachselindh@gmail.com>
2020-01-18 11:04:27 -0600

    Added Portfolio item components and loading hook.

--- client/src/Components/Portfolio/index.js
+++ client/src/Components/Portfolio/index.js
@@ -4,15 +4,17 @@ import "./style.css";
 
 function Portfolio() {
   const [portfolioItems, setPortfolioItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     API.getAllPortfolio()
-      .then(response => setPortfolioItems(response.data))
+      .then(response => setPortfolioItems(response.data), setIsLoading(false))
       .catch(err => console.log(err));
   });
 
   return (
     <div className="col-lg-10 col-sm-12 portfolio-z bg-light">
       <h1>Portfolio</h1>
+
       {portfolioItems.map((item, index) => {
         return <div id={index}>{item.title}</div>;
       })}
