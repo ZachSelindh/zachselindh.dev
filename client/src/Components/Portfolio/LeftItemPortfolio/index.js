a29db05ce69ac731c5c38408dacb74de0c6673cf
Zach Selindh <zachselindh@gmail.com>
2020-01-21 20:15:12 -0600

    Added styling to left item portfolio.

--- client/src/Components/Portfolio/LeftItemPortfolio/index.js
+++ client/src/Components/Portfolio/LeftItemPortfolio/index.js
@@ -1,21 +1,57 @@
 import React from "react";
+import GitHubImg from "../../../assets/icons/nav/github-logo.svg";
+import WebImg from "../../../assets/icons/web-icon.png";
 
 function LeftItemPortfolio(props) {
   return (
     <div id={props.index} className="row portfolio-item">
-      <div className="col-md-12 col-lg-6">
+      <div className="row col-12">
+        <h1 className="portfolio-title">{props.title}</h1>
+      </div>
+      <div className="portfolio-img-div col-lg-6 col-md-12">
         <a href={props.deployed_link} target="#">
           <img
             alt=""
-            className="img-responsive portfolio-button"
+            className="portfolio-button"
             src={props.gif_location}
             width="450px"
             height="250px"
+            align="center"
+            valign="center"
           />
         </a>
       </div>
-      <h1>{props.title}</h1>
-      <p>Left</p>
+      <div className="col-md-12 col-lg-6">
+        <p className="portfolio-body">{props.description}</p>
+      </div>
+      <div className="col-md-12 col-lg-6 link-row">
+        <span className="tech-title"> Links: </span>
+        <a
+          className="deploy-link"
+          href={props.github_link}
+          title="GitHub Repo"
+          target="#"
+        >
+          <img alt="" className="tech-icon" src={GitHubImg} />
+        </a>
+        <a
+          className="deploy-link"
+          href={props.deployed_link}
+          title="Deployed App"
+          target="#"
+        >
+          <img alt="" className="tech-icon" src={WebImg} />
+        </a>
+      </div>
+      <div className="col-md-12 col-lg-6 link-row">
+        <span className="tech-title"> Technologies: </span>
+        <a href={props.github_link} title="GitHub Repo" target="#">
+          <img alt="" className="tech-icon tech-link" src={GitHubImg} />
+        </a>
+        <a href={props.deployed_link} title="Deployed App" target="#">
+          <img alt="" className="tech-icon tech-link" src={WebImg} />
+        </a>
+      </div>
     </div>
   );
 }
