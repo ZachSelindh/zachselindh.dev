cfa1c120e01b26017dda8297f14d3c8ebc4114cb
Zach Selindh <zachselindh@gmail.com>
2020-01-24 21:50:20 -0600

    Added content and styling to home page.

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -3,11 +3,9 @@ import "./style.css";
 
 function About() {
   return (
-    <div className="container">
-      <div className="col-lg-8 col-sm12">
-        <h1>About</h1>
-        <p> Enter your username and password </p>
-      </div>
+    <div className="col-lg-8 col-sm-12 bg-light about-z">
+      <h1>About</h1>
+      <p> Enter your username and password </p>
     </div>
   );
 }
--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -3,11 +3,18 @@ import "./style.css";
 
 function Home() {
   return (
-    <div className="container">
-      <div className="col-lg-8 col-sm12">
-        <h1>About</h1>
-        <p> Enter your username and password </p>
-      </div>
+    <div className="col-lg-8 col-sm-12 bg-light home-z">
+      <h1>Welcome to my Portfolio!</h1>
+      <p>
+        {" "}
+        My name is Zach Selindh, and I am a full-stack developer. I'm certified
+        by the Unversity of Texas at Austin in technologies like React.js,
+        Node.js, mySQL, MongoDB, and more! Click one of the links above to learn
+        more about me and to check out my portfolio.
+        <br></br>
+        <br></br>
+        Thanks for your visit!{" "}
+      </p>
     </div>
   );
 }
