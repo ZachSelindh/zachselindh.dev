4be8623156d554c03d16ed919351a9350408db07
Zach Selindh <zachselindh@gmail.com>
2020-01-24 21:56:38 -0600

    Added styling and contact for the about page.

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -4,8 +4,44 @@ import "./style.css";
 function About() {
   return (
     <div className="col-lg-8 col-sm-12 bg-light about-z">
-      <h1>About</h1>
-      <p> Enter your username and password </p>
+      <h1>About Me</h1>
+      <p>
+        {" "}
+        I was born in 1990 in southern California, and am the oldest of four
+        brothers. From a young age, I have always had an intense curiosity about
+        the world at large and about any specific thing, hobby, or idea that
+        could captivate my attention.
+        <br />
+        <br />
+        As a result, I've worked in many different fields. I've managed a laser
+        tag arena. Worked for a University. Played guitar in a rock band
+        full-time. Worked in fine carpentry and luthiery. But I think, finally,
+        I've found my passion in coding.
+        <br />
+        <br />
+        I'm currently attending a Boot Camp at the University of Texas at
+        Austin. Once I graduate, I'll be jumping headfirst into the world of web
+        and software development.
+        <br />
+        <br />
+        I live in Austin, Texas, with my beautiful wife Nicole and two sweet
+        cats.
+        <br />
+        <br />
+        Please, while you're on my website, check out my{" "}
+        <a className="bodyLink" href="/portfolio">
+          Portfolio
+        </a>{" "}
+        page, and consider perusing my{" "}
+        <a
+          className="bodyLink"
+          target="#"
+          href="https://github.com/ZachSelindh"
+        >
+          GitHub
+        </a>{" "}
+        page as well.
+      </p>
     </div>
   );
 }
