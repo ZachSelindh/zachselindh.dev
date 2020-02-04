3e3fa1774ba1b69caf7aed32711cf6447bcbd296
Zach Selindh <zachselindh@gmail.com>
2020-02-03 22:23:29 -0600

    Refactoring for deploy.

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/public")));
+/* app.use(express.static(path.join(__dirname, "client/public"))); */
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 const routes = require("./routes");
 app.use(routes);
