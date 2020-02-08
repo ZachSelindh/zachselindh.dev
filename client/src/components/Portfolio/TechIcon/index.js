ec6d5a7be2769d101c23083bf6f699b2b9786b32
Zach Selindh <zachselindh@gmail.com>
2020-02-07 21:04:03 -0600

    Added getTechByClick function and removed console logs.

--- client/src/components/Portfolio/TechIcon/index.js
+++ client/src/components/Portfolio/TechIcon/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function TechIcon(props) {
-  console.log(props);
   return <img className="tech-icon" alt={props.name} src={props.location} />;
 }
 
--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -14,31 +14,60 @@ function Portfolio() {
       .catch(err => console.log(err));
   }, []);
 
+  /*  function getTechClick(event) {
+    event.preventDefault();
+    var searchTech = event.target.getAttribute("data").toString();
+    setIsLoading(true);
+    API.getByTech(searchTech)
+      .then(responseTech => setPortfolioItems(responseTech.data))
+      .then(setIsLoading(false))
+      .catch(err => console.log(err));
+  } */
+
   return (
     <div className="col-12 portfolio-z bg-light">
       <h1 id="portfolio-brand-z">Portfolio</h1>
-      {isLoading ? (
-        <div>Loading Portfolio Items...</div>
+      {/* Testing tech search feature */}
+      {/* <div id="search-tech-div" className="col-12">
+        <span id="search-tech-span">
+          {" "}
+          Select a Technology To Refine Results:{" "}
+          <img
+            src="https://www.tuannguyen.tech/wp-content/uploads/2019/01/react.png"
+            alt="React"
+            data="react"
+            className="tech-icon"
+            onClick={getTechClick}
+          ></img>
+        </span>
+      </div> */}
+      {portfolioItems.length ? (
+        isLoading ? (
+          <div>Loading Portfolio Items...</div>
+        ) : (
+          portfolioItems.map((item, index) => {
+            const customProps = {
+              key: item._id,
+              index: index,
+              title: item.title,
+              gif_location: item.gif_location,
+              description: item.description,
+              completed_date: item.completed_date,
+              technologies: item.technologies,
+              github_link: item.github_link,
+              deployed_link: item.deployed_link
+            };
+            return index % 2 === 0 ? (
+              <LeftItemPortfolio {...customProps} />
+            ) : (
+              <RightItemPortfolio {...customProps} />
+            );
+          })
+        )
       ) : (
-        portfolioItems.map((item, index) => {
-          const customProps = {
-            key: item._id,
-            index: index,
-            title: item.title,
-            gif_location: item.gif_location,
-            description: item.description,
-            completed_date: item.completed_date,
-            technologies: item.technologies,
-            github_link: item.github_link,
-            deployed_link: item.deployed_link
-          };
-          return index % 2 === 0 ? (
-            <LeftItemPortfolio {...customProps} />
-          ) : (
-            <RightItemPortfolio {...customProps} />
-          );
-        })
+        <div>No items found!</div>
       )}
+
       <p id="portfolio-footer">Check back later for more apps!</p>
     </div>
   );
